2cb60943b7568ea36c8b65045aa503dd51b041ef
CM <grctestemail@gmail.com>
2021-10-19 22:00:13 +0100

    Update next-sitemap.js

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,4 @@
-import config from "./components/config.json";
+const config = require("./components/config.json");
 
 module.exports = {
   siteUrl: process.env.SITE_URL || `https://${config.domain}`,
